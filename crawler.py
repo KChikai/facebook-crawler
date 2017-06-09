d641537106024d9c326694fc05a351d2b59e3a5d
nearstructure <tokoroten0401@gmail.com>
2017-06-09 22:41:07 +0900

    crawl comments in a group

--- crawler.py
+++ crawler.py
@@ -4,7 +4,26 @@ import facebook
 from access_token import TOKEN
 
 graph = facebook.GraphAPI(TOKEN)
-response = graph.get_object('db.tech.showcase/posts')
 
+# グループidからfeedを取得
+response = graph.get_object(id='116489408559957/feed')
+print('feed数: ', len(response['data']))
+entry_ids = []
 for entry in response['data']:
-    print(entry['message'])
+    entry_ids.append(entry['id'])
+
+# comment内に返信しているcommentを取得
+for entry_id in entry_ids:                                                  # グループ内のEntry ID
+    response = graph.get_object(id=entry_id + '/comments')                  # Entry内のcommentsを取得
+    for entry in response['data']:
+        comment_id = str(entry['id'])
+        response_comments = graph.get_object(id=comment_id + '/comments')   # 各commentに付随するcommentsを取得(response)
+        if len(response_comments['data']) != 0:
+            print('+++ start of talk +++')
+            print(entry['message'])                                         # responseを持っているcommentを表示
+            for comment in response_comments['data']:
+                print(comment['message'])                                   # commentに対するresponseを表示
+                # print(comment)
+            print('+++ end of talk +++')
+            print('')
+    print('------------ end of entry ---------------')
