d967b364e33aecd2e207341477816fbb75c40e0f
nearstructure <tokoroten0401@gmail.com>
2017-06-12 18:05:55 +0900

    fix crawler (crawl ids for each months)

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 facebook から会話データをクロールするスクリプト．
 facebook sdk for pythonを使用．
 
+<br>
+
 # How to create the environment
 
 1. facebook開発者に[ここ](https://developers.facebook.com/docs/apps/register)から登録，
@@ -15,7 +17,9 @@ facebook sdk for pythonを使用．
 ```
     pip install requests
     pip install facebook-sdk
+    pip install datetime
 ```
+
 
 4. `access_token.py`というファイルを`crawler.py`と同じディレクトリ下に作り，以下のように記述．
 
@@ -27,6 +31,7 @@ GROUP_ID = '123456789'      # クロールしたいグループIDを記入（調
 ```
 
 
+<br>
 
 # GET Method
 
@@ -51,15 +56,34 @@ IDが分かったことで，GETメソッドが使える．
 
     http://graph.facebook.com/group_id/feed
 
+新着データのデフォルト数は25個っぽい．それ以上のデータを取りたければ
+`?limit=hoge`のように個数を指定する必要がある．API制限に注意．
+また，UNIX時間を指定することで，指定期間内のデータを取得することができる．
+
 エントリID（投稿ID）からそれに付随するコメント（返信）を取得する．
 
     http://graph.facebook.com/entry_id/comments
 
+<br>
 
 # Request Limit
 
-facebook graph API のリクエスト制限は一分間に600回程度とからしい．
+facebook graph API のリクエスト制限は600秒に600回程度 (per IP) とからしい．
+詳しくは[ここ](https://developers.facebook.com/docs/marketing-api/api-rate-limiting)，
+または[ここ](https://developers.facebook.com/docs/graph-api/advanced/rate-limiting)．
+あまり具体的な明言がなされていない（？）．
 TwitterAPIに比べてゆるい（？）
 
+feedで1000件取得の場合はちゃんとできる．
+feedでのlimit指定を10000件に設定した場合，
+`facebook/__init__.py`でエラーが吐き出される．
 
+    line 269:  raise GraphAPIError('Maintype was not text, image, or querystring')
+
+ちゃんとしたレスポンスが返っていないのでこのようなエラーが返ってくる．
+試しに，raise文を`return response`に書き換えて中身を見る．
+
+    b'{"error":{"code":1,"message":"Please reduce the amount of data you\'re asking for, then retry your request"}}'
+
+量を減らしてや〜と言われている．
 
--- crawler.py
+++ crawler.py
@@ -1,29 +1,49 @@
 # -*- coding: utf-8 -*-
 
 import facebook
+import json
+import datetime
+import time
 from access_token import TOKEN, GROUP_ID
 
 graph = facebook.GraphAPI(TOKEN)
 
 # グループidからfeedを取得
-response = graph.get_object(id=GROUP_ID + '/feed')
-print('feed数: ', len(response['data']))
-entry_ids = []
-for entry in response['data']:
-    entry_ids.append(entry['id'])
+months = [month for month in range(4, 11)]                  # 4~10月のデータを対象にする (プロ野球のシーズン)
+
+for i in range(len(months)):
+
+    # 期間指定
+    begin = datetime.datetime(2016, months[i], 1, 0, 0)
+    end = begin + datetime.timedelta(weeks=4)
+    begin_unix = int(time.mktime(begin.timetuple()))
+    end_unix = int(time.mktime(end.timetuple()))
+
+    # request
+    response = graph.get_object(id=GROUP_ID + '/feed?limit=1000&since='
+                                + str(begin_unix) + '&until=' + str(end_unix), timeout=10)
+    entry_ids = []
+    for entry in response['data']:
+        entry_ids.append(entry['id'])                      # idをlistに入れる
+    print(str(months[i]) + '月 feed数: ', len(entry_ids))
+
+    # save feed's ids (API制限がかかったときのための保険)
+    with open('./crawling_data/entry_ids_' + str(months[i]) + '.list', 'w') as f:
+        json.dump(entry_ids, f)
+
 
 # comment内に返信しているcommentを取得
-for entry_id in entry_ids:                                                  # グループ内のEntry ID
-    response = graph.get_object(id=entry_id + '/comments')                  # Entry内のcommentsを取得
-    for entry in response['data']:
-        comment_id = str(entry['id'])
-        response_comments = graph.get_object(id=comment_id + '/comments')   # 各commentに付随するcommentsを取得(response)
-        if len(response_comments['data']) != 0:
-            print('+++ start of talk +++')
-            print(entry['message'])                                         # responseを持っているcommentを表示
-            for comment in response_comments['data']:
-                print(comment['message'])                                   # commentに対するresponseを表示
-                # print(comment)
-            print('+++ end of talk +++')
-            print('')
-    print('------------ end of entry ---------------')
+# for entry_id in entry_ids:                                                              # グループ内のEntry ID
+#     response = graph.get_object(id=entry_id + '/comments', timeout=10)                  # Entry内のcommentsを取得
+#     for entry in response['data']:
+#         comment_id = str(entry['id'])
+#         response_comments = graph.get_object(id=comment_id + '/comments', timeout=10)   # 各commentに付随するcommentsを取得(response)
+#         if len(response_comments['data']) != 0:
+#             print('+++ start of talk +++')
+#             print(entry['message'])                                                     # responseを持っているcommentを表示
+#             for comment in response_comments['data']:
+#                 print(comment['message'])                                               # commentに対するresponseを表示
+#                 # print(comment)
+#             print('+++ end of talk +++')
+#             print('')
+#     print('------------ end of entry ---------------')
