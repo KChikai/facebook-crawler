1b172a5dccaec63940921ed7f1b95b1b44568ea5
nearstructure <tokoroten0401@gmail.com>
2017-06-17 03:09:32 +0900

    add years

--- conversation_crawler.py
+++ conversation_crawler.py
@@ -12,55 +12,57 @@ graph = facebook.GraphAPI(TOKEN)
 url_regex = re.compile(r"[https?|ftp]://[A-Za-z0-9\-.]{0,62}?\.([A-Za-z0-9\-.]{1,255})/?[A-Za-z0-9.\-?=#%/]*")
 
 # グループidからfeed_ids_listをロード
+years = [2013, 2014, 2015]
 months = [month for month in range(4, 11)]                  # 4~10月のデータを対象にする (プロ野球のシーズン)
 
 request_count = 0
 t0 = time.time()
-for i in range(len(months)):
+for year in years:
+    for month in months:
 
-    # load feed's ids
-    with open('./crawling_data/entry_ids_' + str(months[i]) + '.list', 'r') as f_list:
-        entry_ids = json.load(f_list)
+        # load feed's ids
+        with open('./crawling_data/entry_ids_' + str(year) + '_' + str(month) + '.list', 'r') as f_list:
+            entry_ids = json.load(f_list)
 
-    # comment内に返信しているcommentを取得
-    with open('./crawling_data/conv_' + str(months[i]) + '.txt', 'w') as f:
-        for entry_id in entry_ids:                                                              # グループ内のEntry ID
-            response = graph.get_object(id=entry_id + '/comments', timeout=10)                  # Entry内のcommentsを取得
-            request_count += 1
-            if request_count >= 400:
-                t1 = time.time()
-                rest = 600 - (t1 - t0) if 600 - (t1 - t0) > 0 else 0
-                print('***** sleep *****', rest, '[s]')
-                time.sleep(rest)
-                request_count = 0
-                t0 = time.time()
-
-            for entry in response['data']:
-                comment_id = str(entry['id'])
-                response_comments = graph.get_object(id=comment_id + '/comments', timeout=10)   # 各commentに付随するcommentsを取得(response)
+        # comment内に返信しているcommentを取得
+        with open('./crawling_data/conv_' + str(year) + '_' + str(month) + '.txt', 'w') as f:
+            for entry_id in entry_ids:                                                              # グループ内のEntry ID
+                response = graph.get_object(id=entry_id + '/comments', timeout=10)                  # Entry内のcommentsを取得
                 request_count += 1
-                if request_count >= 400:
+                if request_count >= 300:
                     t1 = time.time()
                     rest = 600 - (t1 - t0) if 600 - (t1 - t0) > 0 else 0
                     print('***** sleep *****', rest, '[s]')
                     time.sleep(rest)
                     request_count = 0
                     t0 = time.time()
-                if len(response_comments['data']) != 0:
-                    print('+++ start of talk +++')
-                    print(entry['message'])                                                     # responseを持っているcommentを表示
-                    post = re.sub(r"(\n|\r\n)",  " ", entry['message'])
-                    m_post = url_regex.search(post)
-                    if m_post is None:
-                        for comment in response_comments['data']:
-                            print(comment['message'])                                           # commentに対するresponseを表示
-                            cmnt = re.sub(r"(\n|\r\n)",  " ", comment['message'])
-                            m_cmnt = url_regex.search(cmnt)
-                            if m_cmnt is None:
-                                f.write(post + '\t' + cmnt + '\n')
-                                post = cmnt
-                            else:
-                                break
-                    print('+++ end of talk +++')
-                    print('')
-            print('------------ end of entry ---------------')
+
+                for entry in response['data']:
+                    comment_id = str(entry['id'])
+                    response_comments = graph.get_object(id=comment_id + '/comments', timeout=10)   # 各commentに付随するcommentsを取得(response)
+                    request_count += 1
+                    if request_count >= 300:
+                        t1 = time.time()
+                        rest = 600 - (t1 - t0) if 600 - (t1 - t0) > 0 else 0
+                        print('***** sleep *****', rest, '[s]')
+                        time.sleep(rest)
+                        request_count = 0
+                        t0 = time.time()
+                    if len(response_comments['data']) != 0:
+                        print('+++ start of talk +++')
+                        print(entry['message'])                                                     # responseを持っているcommentを表示
+                        post = re.sub(r"(\n|\r\n)",  " ", entry['message'])
+                        m_post = url_regex.search(post)
+                        if m_post is None:
+                            for comment in response_comments['data']:
+                                print(comment['message'])                                           # commentに対するresponseを表示
+                                cmnt = re.sub(r"(\n|\r\n)",  " ", comment['message'])
+                                m_cmnt = url_regex.search(cmnt)
+                                if m_cmnt is None:
+                                    f.write(post + '\t' + cmnt + '\n')
+                                    post = cmnt
+                                else:
+                                    break
+                        print('+++ end of talk +++')
+                        print('')
+                print('------------ end of entry ---------------')
--- feed_crawler.py
+++ feed_crawler.py
@@ -9,24 +9,26 @@ from access_token import TOKEN, GROUP_ID
 graph = facebook.GraphAPI(TOKEN)
 
 # グループidからfeedを取得
+years = [2013, 2014, 2015]
 months = [month for month in range(4, 11)]                  # 4~10月のデータを対象にする (プロ野球のシーズン)
 
-for i in range(len(months)):
+for year in years:
+    for month in months:
 
-    # 期間指定
-    begin = datetime.datetime(2016, months[i], 1, 0, 0)
-    end = begin + datetime.timedelta(weeks=4)
-    begin_unix = int(time.mktime(begin.timetuple()))
-    end_unix = int(time.mktime(end.timetuple()))
+        # 期間指定
+        begin = datetime.datetime(year, month, 1, 0, 0)
+        end = begin + datetime.timedelta(weeks=4)
+        begin_unix = int(time.mktime(begin.timetuple()))
+        end_unix = int(time.mktime(end.timetuple()))
 
-    # request
-    response = graph.get_object(id=GROUP_ID + '/feed?limit=1000&since='
-                                + str(begin_unix) + '&until=' + str(end_unix), timeout=10)
-    entry_ids = []
-    for entry in response['data']:
-        entry_ids.append(entry['id'])                      # idをlistに入れる
-    print(str(months[i]) + '月 feed数: ', len(entry_ids))
+        # request
+        response = graph.get_object(id=GROUP_ID + '/feed?limit=1000&since='
+                                    + str(begin_unix) + '&until=' + str(end_unix), timeout=10)
+        entry_ids = []
+        for entry in response['data']:
+            entry_ids.append(entry['id'])                      # idをlistに入れる
+        print(str(month) + '月 feed数: ', len(entry_ids))
 
-    # save feed's ids (API制限がかかったときのための保険)
-    with open('./crawling_data/entry_ids_' + str(months[i]) + '.list', 'w') as f:
-        json.dump(entry_ids, f)
+        # save feed's ids (API制限がかかったときのための保険)
+        with open('./crawling_data/entry_ids_' + str(year) + '_' + str(month) + '.list', 'w') as f:
+            json.dump(entry_ids, f)
